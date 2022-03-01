c70850dc525e1d5ee30cba95f94cb78e7f6b12b0
Augu1sto <yingu081@163.com>
2022-03-01 10:15:05 +0800

    tryui

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -6,8 +6,9 @@ import router from './router'
 import store from './store'
 
 import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
-import 'element-theme-ink/dist/index.css'
+// import 'element-theme-ink/dist/index.css'
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
